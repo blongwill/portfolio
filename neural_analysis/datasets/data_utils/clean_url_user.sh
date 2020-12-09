8e8fdd3a2d92d3a4999ee25038c10c9ff2c5b0e9
Ben Longwill <longwill@uw.edu>
2020-12-09 15:44:30 -0800

    delete old file

--- neural_analysis/datasets/data_utils/clean_url_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-input_file=$1
-output_file=$2
-
-tr -d '' <$input_file >$output_file
-sed -i 's/https\?:\S*/URL/g' $output_file
-sed -i 's/@\S\+/USER/g' $output_file
-
--- neural_analysis/src/main.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python ~/ling575_neural_models/src/main.py $1
